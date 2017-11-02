c0aa705bc4405c373adad15146194a12cd25a6fb
tlnob <thais.nobre7@gmail.com>
2017-11-02 17:47:51 -0200

    add array of points

--- main.jl
+++ main.jl
@@ -1,12 +1,44 @@
+using JuMP
 using PlotRecipes
 using Gurobi
 
-#pyplot()
-
-#posX, posY = [],[]
 
 #for i in sort(collect(keys(citiesDict)))
-	
+
+#posição de cada ponto a ser visitado
+citiesDict = Dict{Int,Any}()
+citiesDict[1] = (523,418)
+citiesDict[2] = (527,566)
+citiesDict[3] = (435,603)
+citiesDict[4] = (386,660)
+citiesDict[5] = (346,692)
+citiesDict[6] = (431,730)
+citiesDict[7] = (419,818)
+citiesDict[8] = (347,520)
+citiesDict[9] = (332,330)
+citiesDict[10] = (165,374)
+citiesDict[11] = (196,198)
+citiesDict[12] = (187,108)
+citiesDict[13] = (210,63)
+
+nCities= length(citiesDict)     
+
+#matriz que armazena distâncias entre pontos
+c = zeros(nCities,nCities)
+
+posX, posY = [],[]
+
+for i in sort(collect(keys(citiesDict)))
+	posI = citiesDict[i]
+	for j in sort(collect(keys(citiesDict)))
+		posJ = citiesDict[j]
+		c[i,j] = ((posI[1]-posJ[1])^2 + 
+			  (posI[2]-posJ[2])^2)^0.5
+	end
+	append!(posX, posI[1])
+	append!(posY, posI[2])
+end
+
 model = Model(solver=GurobiSolver(TimeLimit=20, Threads=1,
 	      Heuristics=0.0, OutputFlag=0))
 
